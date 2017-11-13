6b576e1fb924b776231d7891f2f4c5441c564719
Martin White <m.white@sussex.ac.uk>
2017-11-13 09:41:00 +0000

    Added comments to view

--- mvc/view/explorer.php
+++ mvc/view/explorer.php
@@ -26,13 +26,15 @@
 			</ul>
 		</div>
 	
-
+		<!-- Display selected Bitcoin information from Bitcoin exchnages and current current block -->
 		<div id="information"> 
 			<h3 id="information_header">Blockchain Information</h3>  
 			<div id="information_body">
+				<!-- Grab lastes Bitcoin price from Bitfinex, Bitstamp and Coinbase exchanges -->
 				<p><div class="information_sub_heading">BitFinex Last Price: </div><div id="bitfinex_lp"></div></p>
 				<p><div class="information_sub_heading">Bitstamp Last Price: </div><div id="bitstamp_lp"></div></p>
 				<p><div class="information_sub_heading">Coinbase Last Price: </div><div id="coinbase_lp"></div></p>
+				<!-- Get current block height, bits transacted at this block, miner address who validated this block, current value of Satoshi for this miner, and Bitcoin value for this miner -->
 				<p><div class="information_sub_heading">Current Block Height: </div><div id="current_block_height"></div></p>
 				<p><div class="information_sub_heading">Bits Transacted in this Block: </div><div id="bits_transacted"></div></p>
 				<p><div class="information_sub_heading">Miner Address in this Block: </div><div id="miner_address"></div></p>
